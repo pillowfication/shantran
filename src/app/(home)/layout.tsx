2ddf54bd04fa8f58e290946b5fbc77138fc70056
unknown <markus.tran@incisive.com>
2024-11-27 12:40:41 -0800

    fix styles

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -8,17 +8,19 @@ export const metadata: Metadata = {
 
 export default function HomeLayout ({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto xl:max-w-screen-lg'>
       <nav>
         <ul className='flex w-full [&>li]:inline-block [&>li]:px-2 [&>li]:py-5'>
-          <li className='flex-grow'>SHANNON TRAN</li>
+          <li className='flex-grow'>
+            <span className='hidden md:inline text-lg'>Shannon Tran</span>
+          </li>
           <li><a href='#about-me'>About</a></li>
           <li><a href='#research'>Research</a></li>
           <li><a href='/cv'>CV</a></li>
           <li><a href='#contact'>Contact</a></li>
         </ul>
       </nav>
-      <main className='mb-20'>
+      <main className='mb-20 px-2'>
         {children}
       </main>
     </div>
